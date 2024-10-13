2d1c32fb21bccd7e01f214b6ef55301b4978c2d5
Jordan Patterson <jordan@jpatterson.me>
2024-10-12 22:16:52 -0300

    duh

--- src/BluetoothA2DPSource.cpp
+++ src/BluetoothA2DPSource.cpp
@@ -408,8 +408,8 @@ void BluetoothA2DPSource::filter_inquiry_scan_result(
   }
   /* search for device with MAJOR service class as "rendering" in COD */
   if (!esp_bt_gap_is_valid_cod(cod) ||
-      !(esp_bt_gap_get_cod_srvc(cod) & ESP_BT_COD_SRVC_RENDERING) ||
-      !(esp_bt_gap_get_cod_major_dev(cod) & ESP_BT_COD_MAJOR_DEV_AV)) {
+      (!(esp_bt_gap_get_cod_srvc(cod) & ESP_BT_COD_SRVC_RENDERING) &&
+      !(esp_bt_gap_get_cod_srvc(cod) & ESP_BT_COD_SRVC_AUDIO))) {
     ESP_LOGI(BT_AV_TAG, "--Compatiblity: Incompatible");
     return;
   }
